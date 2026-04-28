121b0528e510826f6a213cc83a2444337ce7b1ce
mmanov <mmanov@adsterra.com>
2026-04-28 18:06:39 +0300

    add middleware for routes

--- internal/controllers/routes.go
+++ internal/controllers/routes.go
@@ -1,16 +1,19 @@
 package controllers
 
-import "net/http"
+import (
+	"net/http"
+	"weather-viewer/internal/middlewares"
+)
 
 func RegisterUserRoutes(mux *http.ServeMux, c *UserController) {
-	mux.Handle("POST /auth/register", http.HandlerFunc(c.RegisterUser))
-	mux.Handle("POST /auth/login", http.HandlerFunc(c.LoginUser))
-	mux.Handle("POST /auth/logout", http.HandlerFunc(c.LogoutUser))
+	mux.Handle("POST /auth/register", middlewares.JSON(http.HandlerFunc(c.RegisterUser)))
+	mux.Handle("POST /auth/login", middlewares.JSON(http.HandlerFunc(c.LoginUser)))
+	mux.Handle("POST /auth/logout", middlewares.JSON(http.HandlerFunc(c.LogoutUser)))
 }
 
 func RegisterLocationRoutes(mux *http.ServeMux, c *LocationsController) {
-	mux.Handle("GET /searchLocation", http.HandlerFunc(c.GetLocation))
-	mux.Handle("POST /addLocation", http.HandlerFunc(c.AddLocation))
-	mux.Handle("GET /getLocations", http.HandlerFunc(c.GetLocations))
-	mux.Handle("DELETE /removeLocation", http.HandlerFunc(c.RemoveLocation))
+	mux.Handle("GET /searchLocation", middlewares.JSON(http.HandlerFunc(c.GetLocation)))
+	mux.Handle("POST /addLocation", middlewares.JSON(http.HandlerFunc(c.AddLocation)))
+	mux.Handle("GET /getLocations", middlewares.JSON(http.HandlerFunc(c.GetLocations)))
+	mux.Handle("DELETE /removeLocation", middlewares.JSON(http.HandlerFunc(c.RemoveLocation)))
 }
