ad80e5692c2796ec59d18c909189c6b9ac6ec852
mmanov <mmanov@adsterra.com>
2026-04-29 17:52:59 +0300

    add tests

--- tests/integration/location_controller_test.go
+++ tests/integration/location_controller_test.go
@@ -4,96 +4,199 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
-	"reflect"
+	"strings"
 	"testing"
 	"weather-viewer/internal/domain"
 	"weather-viewer/internal/dto"
-	testutils2 "weather-viewer/internal/testutils"
+	"weather-viewer/internal/testutils"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
+// GET /searchLocation/{id}
 func TestSearchLocation_success(t *testing.T) {
-	db := testutils2.NewTestDB()
-	app := testutils2.NewTestApp(db)
+	db := testutils.NewTestDB()
+	app := testutils.NewTestApp(db)
 
 	req, err := http.NewRequest(http.MethodGet, "/searchLocation/1", nil)
-	if err != nil {
-		t.Fatalf("http request error: %s", err)
-	}
+	require.NoError(t, err)
 	rr := httptest.NewRecorder()
 
 	app.Server.Mux.ServeHTTP(rr, req)
 
-	if rr.Code != http.StatusOK {
-		t.Fatalf("expected 200, got %d\nbody: %s", rr.Code, rr.Body.String())
-	}
+	testutils.AssertStatus(t, rr, http.StatusOK)
+
 	var got dto.LocationResponse
-	if err := json.NewDecoder(rr.Body).Decode(&got); err != nil {
-		t.Fatalf("faild to decode response: %v", err)
-	}
+	require.NoError(t, json.NewDecoder(rr.Body).Decode(&got))
 
 	expected := dto.LocationResponse{
 		ID: 1, Name: "Москва", UserID: 1, Latitude: 0, Longitude: 0,
 	}
 
-	if !reflect.DeepEqual(got, expected) {
-		t.Fatalf("got: %+v, expected: %+v", got, expected)
-	}
+	assert.Equal(t, expected, got)
 }
 
 func TestSearchLocation_error_incorrectId(t *testing.T) {
-	db := testutils2.NewTestDB()
-	app := testutils2.NewTestApp(db)
+	db := testutils.NewTestDB()
+	app := testutils.NewTestApp(db)
 
 	req, err := http.NewRequest(http.MethodGet, "/searchLocation/aaa", nil)
-	if err != nil {
-		t.Fatalf("http request error: %s", err)
-	}
+	require.NoError(t, err)
+
 	rr := httptest.NewRecorder()
 
 	app.Server.Mux.ServeHTTP(rr, req)
 
-	if rr.Code != http.StatusBadRequest {
-		t.Fatalf("expected 400, got %d\nbody: %s", rr.Code, rr.Body.String())
-	}
+	testutils.AssertStatus(t, rr, http.StatusBadRequest)
+
 	var got domain.ErrorResponse
-	if err := json.NewDecoder(rr.Body).Decode(&got); err != nil {
-		t.Fatalf("decode error: %v\nbody: %s", err, rr.Body.String())
-	}
+	require.NoError(t, json.NewDecoder(rr.Body).Decode(&got))
 
 	expected := domain.ErrorResponse{
 		Message: "Некорректное значение в id",
 	}
 
-	if !reflect.DeepEqual(got, expected) {
-		t.Fatalf("got: %+v, expected: %+v", got, expected)
-	}
+	assert.Equal(t, expected, got)
 }
 
 func TestSearchLocation_error_locationNotFound(t *testing.T) {
-	db := testutils2.NewTestDB()
-	app := testutils2.NewTestApp(db)
+	db := testutils.NewTestDB()
+	app := testutils.NewTestApp(db)
 
 	req, err := http.NewRequest(http.MethodGet, "/searchLocation/244", nil)
-	if err != nil {
-		t.Fatalf("http request error: %s", err)
-	}
+	require.NoError(t, err)
+
 	rr := httptest.NewRecorder()
 
 	app.Server.Mux.ServeHTTP(rr, req)
 
-	if rr.Code != http.StatusNotFound {
-		t.Fatalf("expected 404, got %d\nbody: %s", rr.Code, rr.Body.String())
-	}
+	testutils.AssertStatus(t, rr, http.StatusNotFound)
+
 	var got domain.ErrorResponse
-	if err := json.NewDecoder(rr.Body).Decode(&got); err != nil {
-		t.Fatalf("decode error: %v\nbody: %s", err, rr.Body.String())
-	}
+	require.NoError(t, json.NewDecoder(rr.Body).Decode(&got))
 
 	expected := domain.ErrorResponse{
 		Message: "Данная локация не найдена",
 	}
 
-	if !reflect.DeepEqual(got, expected) {
-		t.Fatalf("got: %+v, expected: %+v", got, expected)
+	assert.Equal(t, expected, got)
+}
+
+// POST /addLocation
+
+func TestAddLocation_success(t *testing.T) {
+	db := testutils.NewTestDB()
+	app := testutils.NewTestApp(db)
+
+	req, err := http.NewRequest(http.MethodPost, "/addLocation", strings.NewReader("name=Тверь&id=1&latitude=3&longitude=4"))
+	require.NoError(t, err)
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	rr := httptest.NewRecorder()
+
+	app.Server.Mux.ServeHTTP(rr, req)
+
+	testutils.AssertStatus(t, rr, http.StatusCreated)
+
+	var got dto.LocationResponse
+	require.NoError(t, json.NewDecoder(rr.Body).Decode(&got))
+
+	assert.NotZero(t, got.ID)
+
+	expected := dto.LocationResponse{
+		ID: 0, Name: "Тверь", UserID: 1, Latitude: 3, Longitude: 4,
 	}
+	got.ID = 0
+	assert.Equal(t, expected, got)
+}
+
+func TestAddLocation_error_invalidFieldValues(t *testing.T) {
+	db := testutils.NewTestDB()
+	app := testutils.NewTestApp(db)
+
+	nameMessage := "Некорректное значение в name"
+	idMessage := "Некорректное значение в id"
+	latitudeMessage := "Некорректное значение в latitude"
+	longitudeMessage := "Некорректное значение в longitude"
+
+	tests := []struct {
+		name    string
+		input   string
+		message string
+	}{
+		{"empty name", "name=&id=1&latitude=3&longitude=4", nameMessage},
+		{"name with spaces for name", "name=  &id=1&latitude=3&longitude=4", nameMessage},
+		{"name with special char for name", "name=...&id=1&latitude=3&longitude=4", nameMessage},
+		{"name with special char for name", "name=!?л?&id=1&latitude=3&longitude=4", nameMessage},
+		{"name with special char for name", "name=^^k^&id=1&latitude=3&longitude=4", nameMessage},
+		{"name with numbers for name", "name=1234d5&id=1&latitude=3&longitude=4", nameMessage},
+
+		{"empty id", "name=Тверь&id=&latitude=3&longitude=4", idMessage},
+		{"id with spaces", "name=Тверь&id= &latitude=3&longitude=4", idMessage},
+		{"id with cyrillic char", "name=Тверь&id=тест&latitude=3&longitude=4", idMessage},
+		{"id with special char", "name=Тверь&id=**3*&latitude=3&longitude=4", idMessage},
+		{"id with english char", "name=Тверь&id=test1&latitude=3&longitude=4", idMessage},
+
+		{"empty latitude", "name=Тверь&id=1&latitude=&longitude=4", latitudeMessage},
+		{"latitude with spaces", "name=Тверь&id=1&latitude=   &longitude=4", latitudeMessage},
+		{"latitude with cyrillic char", "name=Тверь&id=1&latitude=лво12&longitude=4", latitudeMessage},
+		{"latitude with special char", "name=Тверь&id=1&latitude=:3:&longitude=4", latitudeMessage},
+		{"latitude with english char", "name=Тверь&id=1&latitude=abc2&longitude=4", latitudeMessage},
+
+		{"empty longitude", "name=Тверь&id=1&latitude=3&longitude=", longitudeMessage},
+		{"longitude with spaces", "name=Тверь&id=1&latitude=3&longitude=    ", longitudeMessage},
+		{"longitude with cyrillic char", "name=Тверь&id=1&latitude=3&longitude=ю.12", longitudeMessage},
+		{"longitude with english chars", "name=Тверь&id=1&latitude=3&longitude=test", longitudeMessage},
+		{"longitude with special char", "name=Тверь&id=1&latitude=3&longitude=№№2", longitudeMessage},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			req, err := http.NewRequest(http.MethodPost, "/addLocation", strings.NewReader(tt.input))
+			require.NoError(t, err)
+
+			req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+			rr := httptest.NewRecorder()
+
+			app.Server.Mux.ServeHTTP(rr, req)
+
+			testutils.AssertStatus(t, rr, http.StatusBadRequest)
+
+			var got domain.ErrorResponse
+			require.NoError(t, json.NewDecoder(rr.Body).Decode(&got))
+
+			expected := domain.ErrorResponse{
+				Message: tt.message,
+			}
+
+			assert.Equal(t, expected, got)
+		})
+	}
+}
+
+func TestAddLocation_error_locationAlreadyExists(t *testing.T) {
+	db := testutils.NewTestDB()
+	app := testutils.NewTestApp(db)
+
+	req, err := http.NewRequest(http.MethodPost, "/addLocation", strings.NewReader("name=Москва&id=1&latitude=0&longitude=0"))
+	require.NoError(t, err)
+
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	rr := httptest.NewRecorder()
+
+	app.Server.Mux.ServeHTTP(rr, req)
+
+	testutils.AssertStatus(t, rr, http.StatusConflict)
+	var got domain.ErrorResponse
+	require.NoError(t, json.NewDecoder(rr.Body).Decode(&got))
+
+	expected := domain.ErrorResponse{
+		Message: "Такая локация уже существует",
+	}
+
+	assert.Equal(t, expected, got)
 }
