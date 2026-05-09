5141e835ca70e69eabf17fd57d2724141742cd11
mmanov <mmanov@adsterra.com>
2026-05-09 16:07:50 +0300

    add fake repo

---
+++ tests/unit/location_repository.go
@@ -0,0 +1,32 @@
+package unit
+
+import (
+	"weather-viewer/internal/domain"
+	"weather-viewer/internal/interfaces"
+)
+
+type FakeRepository struct {
+	locationRepository interfaces.LocationRepository
+}
+
+func NewFakeRepository() *FakeRepository {
+	return &FakeRepository{}
+}
+
+func (repo *FakeRepository) GetLocation(locationID int) (domain.Location, error) {
+	return domain.Location{
+		ID: 1, Name: "Москва", UserID: 1, Latitude: 0, Longitude: 0,
+	}, nil
+}
+
+func (repo *FakeRepository) AddLocation(location domain.Location) (domain.Location, error) {
+	return domain.Location{}, nil
+}
+
+func (repo *FakeRepository) GetLocations(sessionToken int) ([]domain.Location, error) {
+	return []domain.Location{}, nil
+}
+
+func (repo *FakeRepository) RemoveLocation(id int) error {
+	return nil
+}
