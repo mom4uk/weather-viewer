661247988160d657ac7fed90b7ec2b1d2567432c
mmanov <mmanov@adsterra.com>
2026-05-09 16:07:23 +0300

    add unit test

---
+++ tests/unit/location_service_test.go
@@ -0,0 +1,22 @@
+package unit
+
+import (
+	"testing"
+	"weather-viewer/internal/services"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSearchLocation_success(t *testing.T) {
+	fakeRepo := NewFakeRepository()
+	fakeWeather := NewFakeWeather()
+	service := services.NewLocationService(fakeRepo, fakeWeather)
+	location, err := service.GetLocation(1)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.Equal(t, 1, location.ID)
+	assert.Equal(t, "Москва", location.Name)
+	assert.NotEmpty(t, location.Weather)
+}
