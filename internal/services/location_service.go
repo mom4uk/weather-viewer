867a5853d0791b813e8b69b1382f5aff848e397b
mmanov <mmanov@adsterra.com>
2026-05-09 16:07:41 +0300

    fix abstraction weather

--- internal/services/location_service.go
+++ internal/services/location_service.go
@@ -1,46 +1,43 @@
 package services
 
 import (
-	"encoding/json"
-	"fmt"
-	"log"
-	"net/http"
-	"os"
 	"weather-viewer/internal/domain"
 	"weather-viewer/internal/interfaces"
 )
 
 type LocationService struct {
-	locationRepository interfaces.LocationRepository
+	LocationRepository interfaces.LocationRepository
+	WeatherService     interfaces.Weather
 }
 
-func NewLocationService(locationRepository interfaces.LocationRepository) *LocationService {
+func NewLocationService(locationRepository interfaces.LocationRepository, weatherService interfaces.Weather) *LocationService {
 	return &LocationService{
-		locationRepository: locationRepository,
+		LocationRepository: locationRepository,
+		WeatherService:     weatherService,
 	}
 }
 
 func (s *LocationService) GetLocation(id int) (domain.Location, error) {
-	location, err := s.locationRepository.GetLocation(id)
+	location, err := s.LocationRepository.GetLocation(id)
 	if err != nil {
 		return domain.Location{}, err
 	}
-	weather, err := s.getWeather(location)
+	weather, err := s.WeatherService.GetWeather(location)
 	location.Weather = weather
 	return location, err
 }
 
 func (s *LocationService) AddLocation(location domain.Location) (domain.Location, error) {
-	return s.locationRepository.AddLocation(location)
+	return s.LocationRepository.AddLocation(location)
 }
 
 func (s *LocationService) GetLocations(userID int) ([]domain.Location, error) {
-	locations, err := s.locationRepository.GetLocations(userID)
+	locations, err := s.LocationRepository.GetLocations(userID)
 	if err != nil {
 		return []domain.Location{}, err
 	}
 	for i := range locations {
-		weather, err := s.getWeather(locations[i])
+		weather, err := s.WeatherService.GetWeather(locations[i])
 		if err != nil {
 			return []domain.Location{}, err
 		}
@@ -50,32 +47,5 @@ func (s *LocationService) GetLocations(userID int) ([]domain.Location, error) {
 }
 
 func (s *LocationService) RemoveLocation(id int) error {
-	return s.locationRepository.RemoveLocation(id)
-}
-
-func (s *LocationService) getWeather(location domain.Location) (domain.Weather, error) {
-	apiKey := os.Getenv("API_KEY_WEATHER")
-	url := fmt.Sprintf(
-		"https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s",
-		location.Name,
-		apiKey,
-	)
-	var weather domain.Weather
-
-	resp, err := http.Get(url)
-	if err != nil {
-		return weather, err
-	}
-	defer func() {
-		if err := resp.Body.Close(); err != nil {
-			log.Printf("response close error: %v", err)
-		}
-	}()
-	if resp.StatusCode != http.StatusOK {
-		return weather, fmt.Errorf("weather api error: %s", resp.Status)
-	}
-	if err := json.NewDecoder(resp.Body).Decode(&weather); err != nil {
-		return weather, fmt.Errorf("json decode error")
-	}
-	return weather, nil
+	return s.LocationRepository.RemoveLocation(id)
 }
