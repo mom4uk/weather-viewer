2999e4239a5e4dd207f19a547527f9f19a5c96f0
mmanov <mmanov@adsterra.com>
2026-05-11 21:40:40 +0300

    add init for front

--- server/server.go
+++ server/server.go
@@ -19,7 +19,16 @@ func (s *Server) GetMux() *http.ServeMux {
 	return s.Mux
 }
 
+func (s *Server) InitStatic() {
+	fs := http.FileServer(http.Dir("./static"))
+	s.Mux.Handle("/static/", http.StripPrefix("/static/", fs))
+}
+
 func (s *Server) Start() error {
 	addr := os.Getenv("SERVER_ADDR")
+	if addr == "" {
+		addr = ":8080"
+	}
+
 	return http.ListenAndServe(addr, s.Mux)
 }
